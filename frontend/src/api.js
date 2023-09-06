bd2b9902c2954f2165b255cf453df19efacac7b2
rohitg247 <106257406+rohitg247@users.noreply.github.com>
2023-09-06 22:19:56 +0530

    first commit

---
+++ frontend/src/api.js
@@ -0,0 +1,7 @@
+// frontend/src/api.js
+
+const BASE_URL = 'http://localhost:5000/api'; // Replace with your actual backend URL
+
+export default {
+    todos: `${BASE_URL}/todos`, // Update this with your backend API endpoints
+};
